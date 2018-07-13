5501989fa9e7697e143752ae7c9f565b37678e70
zwy <zhaowenya1995@163.com>
2018-07-13 15:38:22 +0800

    项目第二次提交

--- winxuan/src/App.js
+++ winxuan/src/App.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+// zwy
+import Main from './components/main/main';
+import Classify from './components/classify/classify';
+import Summer from './components/summer/summer';
+import Login from './page/login/login';
+import Register from './page/register/register';
+import Details from './page/details/details';
+import Mine from './page/mine/mine';
+import Favorite_footprint from './page/favorite_footprint/favorite_footprint';
+// drt
+import Goodslist from './components/goodlist/goodslist'
+import Details from './components/details/details'
+import Car from './components/car/car'
+import Settle from './components/settle/settle'
+
+import './assets/css/base.css';
+import './assets/font/iconfont.css';
 import './App.css';
+import 'antd/dist/antd.css';
+
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const store = createStore((state = {
+  count: 1
+},action) => {
+  switch (action.type) {
+      case 'minus':
+      return Object.assign({}, state, {
+          count: action.count,
+      });
+      case 'addition':
+      return Object.assign({}, state, {
+          count: action.count,
+      });
+      
+      default:
+      return state
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Provider store={store}>
+          <div className="App clearfix">
+          {/* zwy */}
+            <Route exact path="/" component={Main}/>
+            <Route path="/classify" component={Classify}></Route>
+            <Route path="/summer" component={Summer}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/register" component={Register}></Route>
+            <Route path="/details/:classname/:proId" component={Details}></Route>
+            <Route path="/mine" component={Mine}></Route>
+            <Route path="/favorite" component={Favorite_footprint}></Route>
+          {/* drt */}
+            <Route exact path="/goodslist/:classify/:classess" component={Goodslist}></Route>
+            <Route path="/details/:tocatch/:bookid" component={Details}></Route>
+            <Route path="/car" component={Car}></Route>
+            <Route path="/settle" component={Settle}></Route>
+          </div>
+        </Provider>
+      </Router>
     );
   }
 }
